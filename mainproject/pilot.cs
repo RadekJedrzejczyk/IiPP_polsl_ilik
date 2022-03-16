901e41fa827a801a36ab68bf3bb3026decc34ef1
Radosław Jędrzejczyk <rj303537@student.polsl.pl>
2022-03-16 11:46:09 +0100

    Dodaj pliki projektów.

---
+++ mainproject/pilot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainproject
+{
+    class pilot
+    {
+        string imie;
+        string nazwisko;
+        int nr_licencji;
+        string rodzaj_uprawnien;
+    }
+}
