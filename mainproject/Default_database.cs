1daad1e1bb52c4c89269960c2d98d2c8271f6169
Marcel170201 <101736343+Marcel170201@users.noreply.github.com>
2022-05-21 18:13:20 +0200

    Update Default_database.cs

--- mainproject/Default_database.cs
+++ mainproject/Default_database.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,54 +18,52 @@ namespace back_end
         protected List<Activity> activity_blocks_list = new List<Activity>();
 
         public void add_to_list(Pilot pilot)
-        { 
+        {
+            users_list.Add(pilot);
 
         }
 
         public void add_to_list(Airship airship)
         {
-
+            airship_list.Add(airship);
         }
 
         public void add_to_list(Procedure procedure)
         {
-
+            procedure_blocks_list.Add(procedure);
         }
 
         public void add_to_list(Activity activity)
         {
-
+            activity_blocks_list.Add((Activity)activity);
         }
 
         public void remove_to_list(Pilot pilot)
         { 
-
+            users_list.Remove(pilot);
         }
 
         public void remove_to_list(Airship airship)
         {
-
+       airship_list.Remove((Airship)airship);
         }
 
         public void remove_to_list(Procedure procedure)
         {
-
+            procedure_blocks_list.Remove(procedure);
         }
 
         public void remove_to_list(Activity activity)
         {
-
+            activity_blocks_list.Remove(activity);
         }
+        public Pilot search(string nazwisko)
+        { 
+         for(int i=0; i<users_list.Count();i++)
+            {
 
-        public void search(Pilot pilot)
-        {
-
+            }
+              return users_list[0];
         }
-
-        public void search(Airship airship)
-        {
-
-        }
-
     }
 }
